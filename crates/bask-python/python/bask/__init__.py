4a759cbb804d518895bd807b624f675437c711f3
Dennis Wuitz <dennish@wuitz.de>
2026-07-09 22:37:13 +0200

    bask: chore: switch copyright to Wavelens GmbH with SPDX headers

---
+++ crates/bask-python/python/bask/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2026 Wavelens GmbH <info@wavelens.io>
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0
---
+++ crates/bask/src/lib.rs
@@ -0,0 +1,5 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Wavelens GmbH <info@wavelens.io>
+ *
+ * SPDX-License-Identifier: MIT OR Apache-2.0
+ */
